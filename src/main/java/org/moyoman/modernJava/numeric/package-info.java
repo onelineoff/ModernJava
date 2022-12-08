e0403e86386728da6e2652b179c9debd5cf13df2
David Weiss <onelineoff@gmail.com>
2022-12-08 16:17:22 -0700

    Remove old code.

--- src/main/java/org/moyoman/modernJava/numeric/package-info.java
+++ src/main/java/org/moyoman/modernJava/numeric/package-info.java
@@ -1,5 +1,9 @@
 /** Solutions to various hackerrank type problems.
  *  The problems which are numeric are gathered here.
  *  There might be a similar package for string manipulation problems.
+ *  
+ *  
  */
 package org.moyoman.modernJava.numeric;
+
+
