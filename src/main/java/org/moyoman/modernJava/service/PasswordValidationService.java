16dfd487a986e2f5238b5530fad94f98c401f51c
David Weiss <onelineoff@gmail.com>
2024-01-19 09:30:07 -0700

    Additional work on password validation service.

--- src/main/java/org/moyoman/modernJava/service/PasswordValidationService.java
+++ src/main/java/org/moyoman/modernJava/service/PasswordValidationService.java
@@ -3,5 +3,9 @@ package org.moyoman.modernJava.service;
 import org.moyoman.modernJava.auth.PasswordValidityEnum;
 
 public interface PasswordValidationService {
+	// TODO These should be autowired, or read from a database.
+	public static final Integer MIN_VALID_PASSWORD = 5;
+	public static final Integer MAX_VALID_PASSWORD = 12;
+
 	public PasswordValidityEnum validatePassword(String password);
 }
